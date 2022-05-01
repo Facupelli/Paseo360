f3c4d7aa94f47448a05474ff9024c882d755b17e
Facupelli <facundopellicer4@gmail.com>
2022-05-01 17:32:20 -0300

    filters styles

--- paseo360/components/PropertyFilter/PropertyFilter.jsx
+++ paseo360/components/PropertyFilter/PropertyFilter.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { departamentos } from "assets/departamentos";
 import s from "./PropertyFilter.module.scss";
 
 export default function PropertyFilter() {
+  const [showMoreFilter, setShowMoreFilters] = useState(false);
+
+  const handleShowMoreFilter = () => {
+    setShowMoreFilters(true);
+  };
+
   return (
     <div className={s.container}>
-      FILTROS
       <form>
         <div>
           <label htmlFor="type">Tipo: </label>
@@ -17,8 +23,6 @@ export default function PropertyFilter() {
             <option value="Quinta">Quinta</option>
             <option value="Otro">Otro</option>
           </select>
-        </div>
-        <div>
           <label htmlFor="operation">Operacion: </label>
           <select id="operation" defaultValue="all">
             <option value="all" disabled>
@@ -27,8 +31,6 @@ export default function PropertyFilter() {
             <option value="Alquiler">Alquiler</option>
             <option value="Venta">Venta</option>
           </select>
-        </div>
-        <div>
           <label htmlFor="departamento">Departamento: </label>
           <select id="departamento" defaultValue="all">
             <option value="all">Todos</option>
@@ -41,22 +43,17 @@ export default function PropertyFilter() {
                   </option>
                 ))}
           </select>
-        </div>
-        <div>
           <label htmlFor="real_estate">Inmobiliaria: </label>
           <select id="real_estate" defaultValue="all">
             <option value="all">Todos</option>
           </select>
         </div>
-        <div>
-          <div>
-            <label htmlFor="price_start">Precio Desde: </label>
-            <input type="text" id="price_start" />
-          </div>
-          <div>
-            <label htmlFor="price_end">Precio Hasta: </label>
-            <input type="text" id="price_end" />
-          </div>
+
+        <div className={s.price_range}>
+          <label htmlFor="price_start">Precio Desde:</label>
+          <input type="text" id="price_start" />
+          <label htmlFor="price_end">Precio hasta:</label>
+          <input type="text" id="price_end" />
           <div>
             <select name="currency">
               <option>Pesos</option>
@@ -64,43 +61,50 @@ export default function PropertyFilter() {
             </select>
           </div>
         </div>
-        <div>
-          <label htmlFor="garage">Año Construcción: </label>
-        </div>
-        <div>
-          <label htmlFor="ambiences">Ambientes: </label>
-        </div>
-        <div>
-          <label htmlFor="bedrooms">Dormitorios: </label>
-        </div>
-        <div>
-          <label htmlFor="bathrooms">Baños: </label>
-        </div>
-        <div>
-          <label htmlFor="garage">Garage: </label>
-        </div>
-        <div>
-          <p>Superficie Total:</p>
-          <div>
-            <label htmlFor="total_area_start">Desde: </label>
-            <input type="text" id="total_area_start" />
-          </div>
-          <div>
-            <label htmlFor="total_area_end">Hasta: </label>
-            <input type="text" id="total_area_end" />
-          </div>
-        </div>
-        <div>
-          <p>Superficie Cubierta:</p>
-          <div>
-            <label htmlFor="cover_area_start">Desde: </label>
-            <input type="text" id="cover_area_start" />
-          </div>
-          <div>
-            <label htmlFor="cover_area_end">Hasta: </label>
-            <input type="text" id="cover_area_end" />
-          </div>
-        </div>
+
+        {!showMoreFilter && <p onClick={handleShowMoreFilter}>Mas Filtros</p>}
+
+        {showMoreFilter && (
+          <>
+            <div>
+              <label htmlFor="garage">Año Construcción: </label>
+            </div>
+            <div>
+              <label htmlFor="ambiences">Ambientes: </label>
+            </div>
+            <div>
+              <label htmlFor="bedrooms">Dormitorios: </label>
+            </div>
+            <div>
+              <label htmlFor="bathrooms">Baños: </label>
+            </div>
+            <div>
+              <label htmlFor="garage">Garage: </label>
+            </div>
+            <div>
+              <p>Superficie Total:</p>
+              <div>
+                <label htmlFor="total_area_start">Desde: </label>
+                <input type="text" id="total_area_start" />
+              </div>
+              <div>
+                <label htmlFor="total_area_end">Hasta: </label>
+                <input type="text" id="total_area_end" />
+              </div>
+            </div>
+            <div>
+              <p>Superficie Cubierta:</p>
+              <div>
+                <label htmlFor="cover_area_start">Desde: </label>
+                <input type="text" id="cover_area_start" />
+              </div>
+              <div>
+                <label htmlFor="cover_area_end">Hasta: </label>
+                <input type="text" id="cover_area_end" />
+              </div>
+            </div>
+          </>
+        )}
       </form>
     </div>
   );
